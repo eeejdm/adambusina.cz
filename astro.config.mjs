aad45135917031b7195238eeb5304f9857913a8a
eeejdm <adam.busina05@gmail.com>
2025-05-25 22:49:57 +0200

    Fix Netlify adapter import and output mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
 import decapCmsOauth from 'astro-decap-cms-oauth';
-import netlify from '@astrojs/netlify/functions';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [decapCmsOauth()],
   adapter: netlify(),
-  output: 'hybrid',
+  output: 'server',
 }); 
